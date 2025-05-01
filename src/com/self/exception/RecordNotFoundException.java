9f582334f031c887e25feadcac7effbc7a4b0584
minho2618 <minho2618@naver.com>
2025-05-01 13:59:01 +0900

    fix : RecordNotFoundException 오타 수정

---
+++ src/com/self/exception/RecordNotFoundException.java
@@ -0,0 +1,10 @@
+package com.self.exception;
+
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException() {
+	};
+
+	public RecordNotFoundException(String message) {
+		super(message);
+	}
+}
